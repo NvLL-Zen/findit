74d6caccd3b473d0011a4c9a646a287e76a8c5c1
NvLL-Zen <nvll.zen@gmail.com>
2025-03-16 17:44:24 +0700

    Added binary search

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+def search(target, start, end):
+
+    if (start > end):
+        return "not found"
+
+    middle = round((start+end)/2)
+
+    if arr[middle] == target:
+        return f"Found at index: {middle}"
+
+    if arr[middle] > target:
+        search(target, start, middle-1)
+
+    if arr[middle] < target:
+        return search(target, middle+1, end)
+
+
+print(search(11, 0, 12))
