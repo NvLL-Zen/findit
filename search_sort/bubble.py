3317846ac21373503f08eae7f51c34b32c0de54a
NvLL-Zen <nvll.zen@gmail.com>
2025-03-17 04:41:49 +0700

    revised bubble sort

--- search_sort/bubble.py
+++ search_sort/bubble.py
@@ -1,25 +1,13 @@
 arr = [7,11,10,5,12,4,18,15]
 
 def bubble_sort(arr):
-  
-    # Outer loop to iterate through the list n times
     for n in range(len(arr)):
-        
-        # Initialize swapped to track if any swaps occur
-        swapped = False  
+        swapped = False
 
-        # Inner loop to compare adjacent elements
         for i in range(len(arr) - n - 1):
-            if arr[i] > arr[i + 1]:
-              
-                # Swap elements if they are in the wrong order
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                
-                
-                # Mark that a swap has occurred
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
                 swapped = True
-        
-        # If no swaps occurred, the list is already sorted
         if not swapped:
             break
     
@@ -27,4 +15,8 @@ def bubble_sort(arr):
 print(f"before {arr}")
 bubble_sort(arr)
 print(f"after {arr}")
+
+counter = 0
+swapped = False
+
 
